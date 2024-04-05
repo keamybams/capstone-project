3432193db484c1448fc1c72056c3244e86a4f4af
Keamybams <rulibams16@gmail.com>
2024-04-05 11:24:50 +0100

    Hero Section of landing page

--- period-slay/src/app/components/LandingPageHeader.jsx
+++ period-slay/src/app/components/LandingPageHeader.jsx
@@ -1,13 +1,16 @@
 import './LandingPageHeader.css'
+import logo from './slaylogo.png'
 
 export const LandingPageHeader = () => {
     return (<header className='header'>
-        <div className='logo'>Logo</div>
+        <div className='logo'>
+            <img src={logo} alt="logo" />
+        </div>
         <div className='navbar'>
             <ul className='navmenu'>
             <li><a href="#home">Home</a></li>
             <li><a href="#home">Information Hub</a></li>
-            <li><a href="#home">Projects</a></li>
+            <li><a href="#home">Privacy Policy</a></li>
             <li><a href="#home">Support</a></li>
             <li><a href="#home">About Us</a></li>
             </ul>
