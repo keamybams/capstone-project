9f236847c7eb01aa76806a3b4a055cc194693f52
Keamybams <rulibams16@gmail.com>
2024-04-28 15:50:21 +0100

    modal

--- period-slay/src/app/LandingPage.jsx
+++ period-slay/src/app/LandingPage.jsx
@@ -4,12 +4,20 @@ import {LandingHeroSection} from './components/LandingHeroSection'
 import {LandingSubHero} from './components/LandingSubHero'
 import { FaqSection } from './components/FaqSection';
 import { Footer } from './components/Footer';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {SignUpModal} from './components/SignUpModal';
 
 
 export const LandingPage = () => {
-  const [showModal, setShowModal] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    const isModalShown = localStorage.getItem('isModalShown');
+    if (!isModalShown) {
+        setShowModal(true);
+        localStorage.setItem('isModalShown', true);
+    }
+  }, []);
 
   const handleCloseModal = () => {
       setShowModal(false);
