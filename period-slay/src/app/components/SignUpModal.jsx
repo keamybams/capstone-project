79521b75f21917a108a483427cc4be8d29fadb4a
Keamybams <rulibams16@gmail.com>
2024-04-28 15:41:28 +0100

    Sign up

--- period-slay/src/app/components/SignUpModal.jsx
+++ period-slay/src/app/components/SignUpModal.jsx
@@ -1,11 +1,21 @@
 import './SignUpModal.css';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle, faApple } from '@fortawesome/free-brands-svg-icons';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 export const SignUpModal = ({ onClose }) => {
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
     return (
-        <div className="modal">
-            <div className="modal-content">
+        <div className="signup-modal">
+            <div className='modal-wrap'>
+            <div className="signup-modal-content">
                 <span className="close" onClick={onClose}>&times;</span>
                 <h2>Join Period Slay Initiative!</h2>
                 <button className="google-signup" id='email-signup'>
@@ -23,11 +33,23 @@ export const SignUpModal = ({ onClose }) => {
                 </div>
                 
                 <form className='signup-form'>
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
+                    <label htmlFor="pmail">Email</label>
+                    <input type="email" placeholder="Name@example.com" />
+                    <label htmlFor="password">Password</label>
+                    <div className="password-input">
+                    <input 
+                        type={showPassword ? "text" : "password"} 
+                        placeholder="*********" 
+                        id='password'
+                    />
+                    <span className="toggle-password" onClick={togglePasswordVisibility}>
+                        <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
+                    </span>
+                    </div>
                     <button type="submit">Sign Up</button>
                 </form>
                 <p>Already have an account? <a href="/login">Log in</a></p>
+            </div>
             </div>
         </div>
     );
